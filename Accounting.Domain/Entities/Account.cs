5c08a9eadbb1bf01ba72333b96529397a9414a5d
rezam62 <rezamashayekhi62@gmail.com>
2022-05-10 13:24:37 +0200

    account test added

--- Accounting.Domain/Entities/Account.cs
+++ Accounting.Domain/Entities/Account.cs
@@ -7,6 +7,9 @@ namespace Accounting.Domain.Entities
         public Account(Guid id, DateTime createdOn, Guid createdBy, DateTime? modifiedOn, Guid? modifiedBy, string name) 
             : base(id, createdOn, createdBy, modifiedOn, modifiedBy)
         {
+            if(string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException($"The {nameof(Name)} is invalid!");
+
             Name = name;
         }
 
